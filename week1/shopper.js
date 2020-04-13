545304c70af87126e3ab5ea98e8b02a7812b888d
Classactg <benjamin.simpson@bryanuniversity.edu>
2020-04-12 17:42:23 -0700

    second commit

---
+++ week1/shopper.js
@@ -0,0 +1,8 @@
+var shopper = {
+    firstName: "Heppa",
+    lastName : "Wig",
+    age      : "61",
+    eyeColor : "green",
+    fullName : function(){return this.firstName + " " + this.lastName;},
+    shoppingCart: ["grapes", "milk", "bread"]
+}
