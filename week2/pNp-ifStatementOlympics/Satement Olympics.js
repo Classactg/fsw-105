5c3f45e6a541303aa4177a84d399559735a3280b
Classactg <benjamin.simpson@bryanuniversity.edu>
2020-04-18 22:17:43 -0700

    completed if Statement Olympics assignment

---
+++ week2/pNp-ifStatementOlympics/Satement Olympics.js
@@ -0,0 +1,61 @@
+// {<IfStatement> <js>
+// (PRELIMINARIES)
+    if(5>3){
+        console.log("IS FIVE GREATER THAN THREE")
+    }
+    var cat="cat" 
+    if(5>cat.length){
+        console.log("IS THE LENGTH OF CAT")
+    }
+    var dog="dog"
+    if(cat.length === dog.length){
+        console.log("CAT IS EQUAL TO DOG")}
+        if(dog.length !== cat.length)
+        console.log("CAT IS NOT EQUAL TO DOG")
+
+// {BRONZE MEDAL}
+var person = {
+    name: "Bobby",
+    age: 25
+    }
+    // console.log(person.age)
+    // if(18>=person.age) ? positive assignment : negative assignment
+    //     console.log("IF TWELVE IS GREATER THAN EIGHTEEN")}
+    if(person.age < 18){
+        console.log(person.name + " IS NOT ALLOWED TO GO TO THE MOVIE")
+    }
+    else{
+        console.log(person.name + " IS ALLOWED TO GO TO THE MOVIE")    
+    }
+        //     console.log("IF TWELVE IS LESS THAN EIGHTEEN")
+    //     }
+    // if(first>18){
+    //     console.log("IF  IS GREATER THAN EIGHTEEN")
+    //     }
+    // if(B===18){
+    //     console.log("IF B IS EQUAL TO EIGHTEEN")
+    // }
+
+// {SILVER MEDAL}
+var one = 1
+    if(one==="1"){console.log("strict")}else if(one=="1"){console.log("abstract")}else{console.log("not equal at all")}
+        // console.log(one=="1")
+        // console.log(1===1)
+        // console.log("1"==="1")
+// }
+
+// [GOLD MEDAL]
+var location ="Dog Town"; 
+var isTheRealDoggieDogtrue = true;
+var animal = {type: "dog"};
+var myValue  ;
+(typeof(myValue)!== 'undefined') ? console.log('variable is defined') : console.log('variable is undefined')
+// console.log("s" > 12)
+// console.log("t" > 5)
+// console.log("i" > 8)
+// console.log("k" > 0)
+var myNum = 16;
+if(myNum % 16);
+console.log("odd")
+console.log("even")
+
