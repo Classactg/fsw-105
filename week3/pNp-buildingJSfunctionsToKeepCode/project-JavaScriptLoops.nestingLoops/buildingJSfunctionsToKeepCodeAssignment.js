6e3376783eaf774c6a9d193f4a5b152a58708446
Classactg <benjamin.simpson@bryanuniversity.edu>
2020-04-25 17:31:19 -0700

    JSloops Assignment

---
+++ week3/pNp-buildingJSfunctionsToKeepCode/project-JavaScriptLoops.nestingLoops/buildingJSfunctionsToKeepCodeAssignment.js
@@ -0,0 +1,93 @@
+var officeItems = ["stapler", "monitor","computer", "desk", "lamp", "computer", "lamp", "stapler", "computer", "computer"]
+var password = 'computer'
+function count(){
+  var officeItemsCount = 0
+  for(var i = 0; i < officeItems.length; i++){
+    if( officeItems[i]=== password){
+      officeItemsCount += 1
+
+      }
+    }console.log(officeItemsCount) 
+}
+
+count()
+// console.log(officeItems[0])
+// console.log(officeItems[1])
+// console.log(officeItems[2])
+// console.log(officeItems[3])
+// console.log(officeItems[4])
+
+
+
+
+{
+if(password === 0){
+  console.log("")
+}
+var peopleWhoWantToSeeMadMaxFuryRoad = [
+  {
+    name: "Mike",
+    age: 12,
+    gender: "male"
+  },{
+    name: "Madeline",
+    age: 80,
+    gender: "female"
+  },{
+    name: "Cheryl",
+    age: 22,
+    gender: "female"
+  },{
+    name: "Sam",
+    age: 30,
+    gender: "male"
+  },{
+    name: "Suzy",
+    age: 4,
+    gender: "female"
+  }]
+  {function isOldEnough(){
+    for (i = 0; i < peopleWhoWantToSeeMadMaxFuryRoad.length; i++){
+      if(peopleWhoWantToSeeMadMaxFuryRoad[i].age >= 18){
+        console.log("old enough");
+    }if (peopleWhoWantToSeeMadMaxFuryRoad[i].age >= 18){
+      console.log("old enough");
+    }
+    else if(peopleWhoWantToSeeMadMaxFuryRoad[i].age < 18){
+      console.log("not old enough");
+  }
+      
+      }
+    }
+  }
+
+  isOldEnough();
+    // console.log(persons.name)
+    // console.log(persons.age)
+    // if(18>=person.age)
+    //   console.log(person.name + "Is old enough to see Mad Max Fury Road")
+    
+  }
+//console.log(peopleWhoWantToSeeMadMaxFuryRoad[i].name  )
+  // console.log("Madeline is old enough. SHE'S good to see Mad Max Fury Road")
+  function evenOrOdd(){
+    for(var i = 0; i < 101; i++){
+      if(i % 2 === 1) {
+        console.log(`${i} is odd`)
+      
+      }
+      else{
+        console.log(`${i} is even`)
+      }
+    }
+  }
+  
+  evenOrOdd()   
+      // {extraCredit}
+// var nums = [3, 15, 5, 1] // "The light is on"
+// var nums = [7, 7, 7, 7, 7] // "The light is on"
+// var nums = [860, 2, 4, 9] // "The light is off"
+var nums = [2, 3, 2]     
+// for(var i = 7; i < button.length; i++){if(button [2, 3, 2]=== toggle)
+        // console.log("The light is on")
+        // console.log("The light is off")}
