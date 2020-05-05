955fcc7a756733fc873cd0a2afeabf7527cec265
Classactg <benjamin.simpson@bryanuniversity.edu>
2020-05-04 17:53:42 -0700

    calculatorJavascriptNodejs

--- week4/calculator/CalculatorJS.js
+++ week4/calculator/CalculatorJS.js
@@ -1,20 +1,38 @@
-var readlineSync = require('readline-sync')
-function sum(a){
-    return a + a
-}
-console.log(sum(5));
+var readlineSync = require("readline-sync");
 
-function sum(a){
-    return a * a 
-}
-console.log(sum(6));
+     
+var firstNum = readlineSync.questionInt("Please enter your first number: ");
+var secondNum = readlineSync.questionInt("Please enter your second number: ");
+var operation = readlineSync.question("What operation would you like? (enter add, subtract, multiply, divide, or quit)");
 
-function sum(a){
-    return a / a
+// Set up calculator functions
+function add(num1, num2){
+    return "Adding" + num1 + "and" + num2 + "equals: " + (num1 + num2);
 }
-console.log(sum(1));
+function sub(num1, num2){
+    return "Subtracting" + num1 + "from"+ num2 + "equals" + (num1 - num2);
+}
+function mul(num1, num2){
+    return "Multiplying" + num1 + "and" + num2 + "equals:" + (num1 * num2);
+}
+function div(num1, num2){
+    return "Dividing" + num1 + "by" + "equals:" + (num1 / num2);
+}
 
-function sum(a){
-    return a - a
+// Set up while loop to interact with user and display results
+
+if (operation == "add"){
+    console.log(add(firstNum, secondNum))
 }
-console.log(sum(0));
+else if (operation == "subtration"){
+    console.log(sub(firstNum, secondNum))
+}
+else if (operation == "multiply"){
+    console.log(mul(firstNum, secondNum))
+}
+else if (operation == "divide"){
+    console.log(div(firstNum, secondNum))
+}
+else if (operation == "quit"){
+    console.log("Thank You For Playing!");
+}
